46e5a05a81b6135e42611374beafe31a83ae4630
ricomortus <38750470+ricomortus@users.noreply.github.com>
2022-09-25 13:01:42 +0300

    finalise build.

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 let operator = '';
 let displayValue = '';
 let num1 = 0;
@@ -76,15 +74,17 @@ function divide (num1, num2) {
         return displayValue = num1 / num2;
     }
 };
-
-
-
 //FUNCTION BUTTONS
 //Determine result font size based on number of digits
+
+//GO THROUGH STEP BY STEP OF THE CONDITIONS
 function displayFinalResult () {
     if (displayValue.length > 0 && String(operate(operator, num1, num2)).length > 13) {
         displayResultSmall();
-    } else if (displayValue.length === 0) {
+    } else if (displayValue.length > 0 && String(operate(operator, num1, num2)).length < 13) {
+        displayResult();
+    //Need to find a way to differentiate between empty string and 0
+    } else if (String(displayValue.length) == 0) {
         displayText.innerText = '0'
     } else {
         displayResult();
@@ -92,12 +92,20 @@ function displayFinalResult () {
 };
 //Equal button
 equalButton = document.querySelector('.equal-button');
-equalButton.addEventListener('click', () => {
+equalButton.addEventListener('mousedown', () => {
+    equalButton.style.cssText = 'background-color: #FFEBFE'
+});
+equalButton.addEventListener('mouseup', () => {
+    equalButton.style.cssText = 'background-color: #ffd7fe'
     displayFinalResult();
 });
 //Add Button
 addButton = document.querySelector('.add-button');
-addButton.addEventListener('click', () => {
+addButton.addEventListener('mousedown', () => {
+    addButton.style.cssText = 'background-color: #FFBAEE'
+});
+addButton.addEventListener('mouseup', () => {
+    addButton.style.cssText = 'background-color: #ffa7e9'
     displayFinalResult();
     num1 = Number(displayValue);
     operator = 'add';
@@ -105,7 +113,11 @@ addButton.addEventListener('click', () => {
 });
 //Subtract Button
 subtractButton = document.querySelector('.subtract-button');
-subtractButton.addEventListener('click', () => {
+subtractButton.addEventListener('mousedown', () => {
+    subtractButton.style.cssText = 'background-color: #FFBAEE'
+});
+subtractButton.addEventListener('mouseup', () => {
+    subtractButton.style.cssText = 'background-color: #ffa7e9'
     displayFinalResult();
     num1 = Number(displayValue);
     operator = 'subtract';
@@ -113,7 +125,11 @@ subtractButton.addEventListener('click', () => {
 });
 //Multiply Button
 multiplyButton = document.querySelector('.multiply-button');
-multiplyButton.addEventListener('click', () => {
+multiplyButton.addEventListener('mousedown', () => {
+    multiplyButton.style.cssText = 'background-color: #FFBAEE'
+});
+multiplyButton.addEventListener('mouseup', () => {
+    multiplyButton.style.cssText = 'background-color: #ffa7e9'
     displayFinalResult();
     num1 = Number(displayValue);
     operator = 'multiply';
@@ -121,7 +137,11 @@ multiplyButton.addEventListener('click', () => {
 });
 //Divide Button
 divideButton = document.querySelector('.divide-button');
-divideButton.addEventListener('click', () => {
+divideButton.addEventListener('mousedown', () => {
+    divideButton.style.cssText = 'background-color: #FFBAEE'
+});
+divideButton.addEventListener('mouseup', () => {
+    divideButton.style.cssText = 'background-color: #ffa7e9'
     displayFinalResult();
     num1 = Number(displayValue);
     operator = 'divide';
@@ -129,7 +149,11 @@ divideButton.addEventListener('click', () => {
 });
 //All Clear
 allClearButton = document.querySelector('.all-clear-button');
-allClearButton.addEventListener('click', () => {
+allClearButton.addEventListener('mousedown', () => {
+    allClearButton.style.cssText = 'background-color: #FFEBFE'
+});
+allClearButton.addEventListener('mouseup', () => {
+    allClearButton.style.cssText = 'background-color: #ffd7fe'
     displayValue = '';
     num1 = 0;
     num2 = 0;
@@ -139,7 +163,11 @@ allClearButton.addEventListener('click', () => {
 });
 //Clear button
 clearButton = document.querySelector('.clear-button');
-clearButton.addEventListener('click', () => {
+clearButton.addEventListener('mousedown', () => {
+    clearButton.style.cssText = 'background-color: #FFEBFE'
+});
+clearButton.addEventListener('mouseup', () => {
+    clearButton.style.cssText = 'background-color: #ffd7fe'
     displayValue = ''
     displayText.style.cssText = 'font-size: 45px';
     return displayText.innerText = '0';
@@ -148,7 +176,10 @@ clearButton.addEventListener('click', () => {
 //NUMBER BUTTONS
 //Decimal
 decimalButton = document.querySelector('.decimal-button');
-decimalButton.addEventListener('click', () => {
+decimalButton.addEventListener('mousedown', () => decimalButton.style.cssText = 'background-color: #E3E3FF');
+
+decimalButton.addEventListener('mouseup', () => {
+    decimalButton.style.cssText = 'background-color: #D2DAFF';
    //check if decimal exists
    if (!displayValue.includes('.') && displayValue[0] == '0') {
        displayValue += '.'
@@ -159,11 +190,13 @@ decimalButton.addEventListener('click', () => {
         displayValue+= '.'
         return displayText.innerText = displayValue;
    }
-   return displayText.innerText = displayValue;
 });
 //0 
 zeroButton = document.querySelector('.zero-button');
-zeroButton.addEventListener('click', () => {
+zeroButton.addEventListener('mousedown', () => zeroButton.style.cssText = 'background-color: #E3E3FF');
+
+zeroButton.addEventListener('mouseup', () => {
+    zeroButton.style.cssText = 'background-color: #D2DAFF';
     if (displayValue.length < 13 && displayValue[0] !== '0') {
         displayValue += '0';
         return displayText.innerText = displayValue;
@@ -195,28 +228,64 @@ function numberButton (num) {
 };
 //1
 oneButton = document.querySelector('.one-button');
-oneButton.addEventListener('click', () => numberButton('1'));
+oneButton.addEventListener('mousedown', () => oneButton.style.cssText = 'background-color: #E3E3FF');
+oneButton.addEventListener('mouseup', () => {
+    oneButton.style.cssText = 'background-color: #D2DAFF'
+    numberButton('1')
+});
 //2
 twoButton = document.querySelector('.two-button');
-twoButton.addEventListener('click', () => numberButton('2'));
+twoButton.addEventListener('mousedown', () => twoButton.style.cssText = 'background-color: #E3E3FF');
+twoButton.addEventListener('mouseup', () => {
+    twoButton.style.cssText = 'background-color: #D2DAFF'
+    numberButton('2')
+});
 //3
 threeButton = document.querySelector('.three-button');
-threeButton.addEventListener('click', () => numberButton('3'));
+threeButton.addEventListener('mousedown', () => threeButton.style.cssText = 'background-color: #E3E3FF');
+threeButton.addEventListener('mouseup', () => {
+    threeButton.style.cssText = 'background-color: #D2DAFF'
+    numberButton('3')
+});
 //4
 fourButton = document.querySelector('.four-button');
-fourButton.addEventListener('click', () => numberButton('4'));
+fourButton.addEventListener('mousedown', () => fourButton.style.cssText = 'background-color: #E3E3FF');
+fourButton.addEventListener('mouseup', () => {
+    fourButton.style.cssText = 'background-color: #D2DAFF'
+    numberButton('4')
+});
 //5
 fiveButton = document.querySelector('.five-button');
-fiveButton.addEventListener('click', () => numberButton('5'));
+fiveButton.addEventListener('mousedown', () => fiveButton.style.cssText = 'background-color: #E3E3FF');
+fiveButton.addEventListener('mouseup', () => {
+    fiveButton.style.cssText = 'background-color: #D2DAFF'
+    numberButton('5')
+});
 //6
 sixButton = document.querySelector('.six-button');
-sixButton.addEventListener('click', () => numberButton('6'));
+sixButton.addEventListener('mousedown', () => sixButton.style.cssText = 'background-color: #E3E3FF');
+sixButton.addEventListener('mouseup', () => {
+    sixButton.style.cssText = 'background-color: #D2DAFF'
+    numberButton('6')
+});
 //7
 sevenButton = document.querySelector('.seven-button');
-sevenButton.addEventListener('click', () => numberButton('7'));
+sevenButton.addEventListener('mousedown', () => sevenButton.style.cssText = 'background-color: #E3E3FF');
+sevenButton.addEventListener('mouseup', () => {
+    sevenButton.style.cssText = 'background-color: #D2DAFF'
+    numberButton('7')
+});
 //8
 eightButton = document.querySelector('.eight-button');
-eightButton.addEventListener('click', () => numberButton('8'));
+eightButton.addEventListener('mousedown', () => eightButton.style.cssText = 'background-color: #E3E3FF');
+eightButton.addEventListener('mouseup', () => {
+    eightButton.style.cssText = 'background-color: #D2DAFF'
+    numberButton('8')
+});
 //9
 nineButton = document.querySelector('.nine-button');
-nineButton.addEventListener('click', () => numberButton('9'));
+nineButton.addEventListener('mousedown', () => nineButton.style.cssText = 'background-color: #E3E3FF');
+nineButton.addEventListener('mouseup', () => {
+    nineButton.style.cssText = 'background-color: #D2DAFF'
+    numberButton('9')
+});
